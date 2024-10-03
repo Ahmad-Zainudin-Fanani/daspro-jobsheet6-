bdf3452bb447c2bb8d59ab1cc4899726d5cb3b4c
Ahmad-Zainudin-Fanani <azfann.natinons@gmail.com>
2024-10-03 18:39:12 +0700

    memodifikasi program tahun kabisat

--- Pemilihan2Percobaan102.java
+++ Pemilihan2Percobaan102.java
@@ -3,17 +3,25 @@ import java.util.Scanner;
 public class Pemilihan2Percobaan102 {
 
     public static void main(String[] args) {
-        //Deklarasi scanner
+        // Deklarasi scanner
         Scanner input02 = new Scanner(System.in);
 
         System.out.print("Masukkan tahun: ");
         int tahun = input02.nextInt();
 
         if ((tahun % 4) == 0) {
-            if ((tahun % 100) != 0)
+            if ((tahun % 100) == 0) {
+                if ((tahun % 400) == 0) {
+                    System.out.println("Tahun Kabisat");
+                } else {
+                    System.out.println("Bukan Tahun Kabisat");
+                }
+            } else {
                 System.out.println("Tahun Kabisat");
-        } else
+            }
+        } else {
             System.out.println("Bukan Tahun Kabisat");
+        }
 
         input02.close();
     }
